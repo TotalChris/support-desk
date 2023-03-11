eb5893ac3fe4b1d885c7f59f1f4d4f0793276bb3
Chris Yates <chris.m.yates@outlook.com>
2023-03-11 13:52:55 -0500

    Add db connection to Mongo
    - Add init function to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const colors = require('colors')
 const dotenv = require('dotenv').config();
-const {errorHandler} = require('./middleware/errorMiddleware')
+const {errorHandler} = require('./middleware/errorMiddleware');
+const {connectDB} = require('./config/db.js')
 
 const SERVERPORT = process.env.PORT || 8080
+
+//connect to Database
+connectDB();
+
 const app = express();
 
 app.use(express.json());
