3bf258618e89c0ead55a6ca7af7b0e49f5d33f85
Chris Yates <chris.m.yates@outlook.com>
2023-03-13 11:51:09 -0400

    Add Ticket creation and retrieval
    B - Add code to insert and retrieve tickets for a given user in ticketController.js

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -7,14 +7,47 @@ const Ticket = require('../models/ticketModel');
 // @route   GET /api/tickets
 // @access  Private
 const getTickets = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'getTickets'})
+
+    //get user from token
+    const user = await User.findById(req.user.id)
+
+    if(!user){
+        res.status(401);
+        throw new Error('User not authenticated or not found')
+    }
+
+    const tickets = await Ticket.find({user: req.user.id})
+
+    res.status(200).json(tickets)
 })
 
 // @desc    Get user tickets
 // @route   POST /api/tickets
 // @access  Private
 const createTicket = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'createTicket'})
+
+    const {product, description} = req.body;
+
+    if(!product || ! description){
+        res.status(400);
+        throw new Error('Please include all required fields');
+    }
+
+    const user = await User.findById(req.user.id)
+
+    if(!user){
+        res.status(401);
+        throw new Error('User not authenticated or not found')
+    }
+
+    const ticket = await Ticket.create({
+        product,
+        description,
+        user: req.user.id,
+        status: 'pending'
+    })
+
+    res.status(200).json(ticket)
 })
 
 module.exports = {
